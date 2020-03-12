2f3cad1f698d721fe3203c6e4b0cf36838c743b2
ghecko <ghecko78@gmail.com>
2020-03-12 11:59:43 +0100

    add header

--- owfmodules/spi/flash_write.py
+++ owfmodules/spi/flash_write.py
@@ -1,3 +1,11 @@
+# -*- coding:utf-8 -*-
+
+# Octowire Framework
+# Copyright (c) Jordan Ovrè / Paul Duncan
+# License: GPLv3
+# Paul Duncan / Eresse <eresse@dooba.io>
+# Jordan Ovrè / Ghecko <ghecko78@gmail.com
+
 import codecs
 import shutil
 import struct
