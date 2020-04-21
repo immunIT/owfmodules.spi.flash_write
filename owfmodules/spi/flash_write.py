7d158c054f6034f499bb16c1b6477461de0ffda7
ghecko <ghecko78@gmail.com>
2020-04-21 21:56:46 +0200

    Update package version

--- owfmodules/spi/flash_write.py
+++ owfmodules/spi/flash_write.py
@@ -21,7 +21,7 @@ class FlashWrite(AModule):
         super(FlashWrite, self).__init__(owf_config)
         self.meta.update({
             'name': 'SPI write flash',
-            'version': '1.0.0',
+            'version': '1.0.1',
             'description': 'Module to write an SPI flash',
             'author': 'Jordan Ovrè <ghecko78@gmail.com> / Paul Duncan <eresse@dooba.io>'
         })
