5cb0def4a203ce4f0a2c89e922c996543f751c2e
ghecko <ghecko78@gmail.com>
2020-04-22 15:47:22 +0200

    Change options format

--- owfmodules/spi/flash_write.py
+++ owfmodules/spi/flash_write.py
@@ -21,30 +21,30 @@ class FlashWrite(AModule):
         super(FlashWrite, self).__init__(owf_config)
         self.meta.update({
             'name': 'SPI write flash',
-            'version': '1.0.1',
+            'version': '1.0.0',
             'description': 'Module to write an SPI flash',
             'author': 'Jordan Ovrè <ghecko78@gmail.com> / Paul Duncan <eresse@dooba.io>'
         })
-        self.options = [
-            {"Name": "spi_bus", "Value": "", "Required": True, "Type": "int",
-             "Description": "The octowire SPI device (0=SPI0 or 1=SPI1)", "Default": 0},
-            {"Name": "cs_pin", "Value": "", "Required": True, "Type": "int",
-             "Description": "The octowire GPIO used as chip select (CS)", "Default": 0},
-            {"Name": "firmware", "Value": "", "Required": True, "Type": "file_r",
-             "Description": "The firmware to write to the SPI flash memory", "Default": ""},
-            {"Name": "start_chunk", "Value": "", "Required": True, "Type": "int",
-             "Description": "The starting chunk address (1 chunk = 256 bytes)", "Default": 0},
-            {"Name": "spi_baudrate", "Value": "", "Required": True, "Type": "int",
-             "Description": "set SPI baudrate (1000000 = 1MHz) maximum = 50MHz", "Default": 1000000},
-            {"Name": "spi_polarity", "Value": "", "Required": True, "Type": "int",
-             "Description": "set SPI polarity (1=high or 0=low)", "Default": 0},
-            {"Name": "spi_phase", "Value": "", "Required": True, "Type": "string",
-             "Description": "set SPI phase (1=high or 0=low)", "Default": 0}
-        ]
-        self.advanced_options.append(
-            {"Name": "chunk_size", "Value": "", "Required": True, "Type": "int",
-             "Description": "Flash page/sector size", "Default": 0x0100}
-        )
+        self.options = {
+            "spi_bus": {"Value": "", "Required": True, "Type": "int",
+                        "Description": "The octowire SPI device (0=SPI0 or 1=SPI1)", "Default": 0},
+            "cs_pin": {"Value": "", "Required": True, "Type": "int",
+                       "Description": "The octowire GPIO used as chip select (CS)", "Default": 0},
+            "firmware": {"Value": "", "Required": True, "Type": "file_r",
+                         "Description": "The firmware to write to the SPI flash memory", "Default": ""},
+            "start_chunk": {"Value": "", "Required": True, "Type": "int",
+                            "Description": "The starting chunk address (1 chunk = 256 bytes)", "Default": 0},
+            "spi_baudrate": {"Value": "", "Required": True, "Type": "int",
+                             "Description": "set SPI baudrate (1000000 = 1MHz) maximum = 50MHz", "Default": 1000000},
+            "spi_polarity": {"Value": "", "Required": True, "Type": "int",
+                             "Description": "set SPI polarity (1=high or 0=low)", "Default": 0},
+            "spi_phase": {"Value": "", "Required": True, "Type": "string",
+                          "Description": "set SPI phase (1=high or 0=low)", "Default": 0}
+        }
+        self.advanced_options.update({
+            "chunk_size": {"Value": "", "Required": True, "Type": "int",
+                           "Description": "Flash page/sector size", "Default": 0x0100}
+        })
         self.t_width, _ = shutil.get_terminal_size()
 
     @staticmethod
@@ -81,14 +81,14 @@ class FlashWrite(AModule):
         self.wait_status(spi_instance, cs)
 
     def writing_process(self):
-        bus_id = self.get_option_value("spi_bus")
-        cs_pin = self.get_option_value("cs_pin")
-        spi_baudrate = self.get_option_value("spi_baudrate")
-        spi_cpol = self.get_option_value("spi_polarity")
-        spi_cpha = self.get_option_value("spi_phase")
-        current_chunk_addr = self.get_option_value("start_chunk")
-        firmware = self.get_option_value("firmware")
-        chunk_size = self.get_advanced_option_value("chunk_size")
+        bus_id = self.options["spi_bus"]["Value"]
+        cs_pin = self.options["cs_pin"]["Value"]
+        spi_baudrate = self.options["spi_baudrate"]["Value"]
+        spi_cpol = self.options["spi_polarity"]["Value"]
+        spi_cpha = self.options["spi_phase"]["Value"]
+        current_chunk_addr = self.options["start_chunk"]["Value"]
+        firmware = self.options["firmware"]["Value"]
+        chunk_size = self.advanced_options["chunk_size"]["Value"]
 
         t_width, _ = shutil.get_terminal_size()
 
