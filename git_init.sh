9e61fa535c253e3abe581a6f05f9f3a1ab583e05
ghecko <ghecko78@gmail.com>
2020-06-30 14:46:15 +0200

    Change https url to git url

--- git_init.sh
+++ git_init.sh
@@ -1,3 +1,3 @@
-git remote set-url origin --push --add https://github.com/immunIT/owfmodules.spi.flash_write
-git remote set-url origin --push --add https://bitbucket.org/octowire/owfmodules.avrisp.flash_write
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.spi.flash_write.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.spi.flash_write.git
 git remote -v
