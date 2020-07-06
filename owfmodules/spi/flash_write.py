4282a744e9cd23186a6279090b5b4836bd493d9b
Eresse <eresse@dooba.io>
2020-07-06 15:04:40 +0200

    Fixed typos

--- owfmodules/spi/flash_write.py
+++ owfmodules/spi/flash_write.py
@@ -107,8 +107,6 @@ class FlashWrite(AModule):
                     data = f.read(chunk_size)
                     if not data:
                         break
-                    print(" " * t_width, end="\r", flush=True)
-                    print("[*] Writing to address: {}".format(hex(current_chunk_addr)), end="\r", flush=True)
                     self.write_flash(flash_interface, cs, data, current_chunk_addr)
                     current_chunk_addr += chunk_size
                 self.logger.handle("Done!", self.logger.SUCCESS)
